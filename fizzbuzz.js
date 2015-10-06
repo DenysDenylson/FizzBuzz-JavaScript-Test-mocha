52011cae5f0b23e294eb292715ef2a3b7e2c0aa5
DenysDenylson <DenysDenylson@gmail.com>
2015-10-06 00:47:12 -0400

    Cuarto tiempo

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -6,28 +6,32 @@ describe('Fizzbuzz', function () {
 			assert.equal("1", fizzbuzz(1));
 		});
 
-		// it('should return fizz si es multiplo de 3 ', function () {
-		// 	assert.equal("Fizz", fizzbuzz(9));
-		// 	assert.equal("Fizz", fizzbuzz(21));
-		// });
+		it('should return fizz si es multiplo de 3 ', function () {
+			assert.equal("Fizz", fizzbuzz(9));
+			assert.equal("Fizz", fizzbuzz(21));
+		});
 
-		// it('should return Buzz si es multiplo de 5 ', function () {
-		// 	assert.equal("Buzz", fizzbuzz(5));
+		it('should return Buzz si es multiplo de 5 ', function () {
+			assert.equal("Buzz", fizzbuzz(5));
+		});
 
-		// });
-
-		// it('should return Fizz Buzz si es multiplo de 3 y 5 ', function () {
-		// 	assert.equal("Fizz Buzz", fizzbuzz(15));
-		// 	assert.equal("Fizz Buzz", fizzbuzz(45));
-		// });
+		it('should return Fizz Buzz si es multiplo de 3 y 5 ', function () {
+			assert.equal("Fizz Buzz", fizzbuzz(15));
+			assert.equal("Fizz Buzz", fizzbuzz(45));
+		});
 	});
 });
 
 var fizzBuzz1 = {
-
-	// esFizz : function esmultiplode3 () {
-	// 	return (this.number % 3) === 0;
-	// },
+	esFizz : function esmultiplode3 () {
+		if ((this.number % 3) === 0)
+			return true;
+		else
+			return false;
+	},
+	esBuzz : function esmultiplode5 (){
+		return ((this.number % 5) === 0)	
+	},
 	number: 0
 
 	// mensaje: ""
@@ -47,15 +51,17 @@ var fizzBuzz1 = {
 
 var fizzbuzz = function (n) {
 	fizzBuzz1.number = n;
-	// if (fizzBuzz1.esFizz && esmultiplode5(n)) {
-	// 	return "Fizz Buzz"
-	// }
-	if (fizzBuzz1.esFizz) {
+	
+	if (fizzBuzz1.esFizz() && fizzBuzz1.esBuzz()) {
+		return "Fizz Buzz"
+	}
+	
+	if (fizzBuzz1.esFizz()) {
 		return "Fizz";
 	}
-	// if (esmultiplode5(n)) {
-	// 	return "Buzz";
-	// }
+	if (fizzBuzz1.esBuzz()) {
+		return "Buzz";
+	}
 
 	return fizzBuzz1.number;
 };
