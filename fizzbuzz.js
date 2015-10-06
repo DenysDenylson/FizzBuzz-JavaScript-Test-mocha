e9b357b769793ac9331b0389a5d6c138514b3a0d
Leonardo <leo_mejia_p@hotmail.com>
2015-10-06 00:17:04 -0400

    agregando number to fizzbuzz javascrit object

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -6,29 +6,29 @@ describe('Fizzbuzz', function () {
 			assert.equal("1", fizzbuzz(1));
 		});
 
-		it('should return fizz si es multiplo de 3 ', function () {
-			assert.equal("Fizz", fizzbuzz(9));
-			assert.equal("Fizz", fizzbuzz(21));
-		});
+		// it('should return fizz si es multiplo de 3 ', function () {
+		// 	assert.equal("Fizz", fizzbuzz(9));
+		// 	assert.equal("Fizz", fizzbuzz(21));
+		// });
 
-		it('should return Buzz si es multiplo de 5 ', function () {
-			assert.equal("Buzz", fizzbuzz(5));
+		// it('should return Buzz si es multiplo de 5 ', function () {
+		// 	assert.equal("Buzz", fizzbuzz(5));
 
-		});
+		// });
 
-		it('should return Fizz Buzz si es multiplo de 3 y 5 ', function () {
-			assert.equal("Fizz Buzz", fizzbuzz(15));
-			assert.equal("Fizz Buzz", fizzbuzz(45));
-		});
+		// it('should return Fizz Buzz si es multiplo de 3 y 5 ', function () {
+		// 	assert.equal("Fizz Buzz", fizzbuzz(15));
+		// 	assert.equal("Fizz Buzz", fizzbuzz(45));
+		// });
 	});
 });
 
 var fizzBuzz1 = {
 
-	get : function esmultiplode3 (n) {
-		return n % 3 === 0;
-	}
-	
+	// esFizz : function esmultiplode3 () {
+	// 	return (this.number % 3) === 0;
+	// },
+	number: 0
 
 	// mensaje: ""
 	// esFizz: function esMultiploDe(number, multiple){
@@ -41,20 +41,21 @@ var fizzBuzz1 = {
 // 	return n % 3 === 0;
 // };
 
-var esmultiplode5 = function (n) {
-	return n % 5 === 0;
-};
+// var esmultiplode5 = function (n) {
+// 	return n % 5 === 0;
+// };
 
 var fizzbuzz = function (n) {
-	if (fizzbuzz1.get(n) && esmultiplode5(n)) {
-		return "Fizz Buzz"
-	}
-	if (fizzbuzz1.get(n)) {
+	fizzBuzz1.number = n;
+	// if (fizzBuzz1.esFizz && esmultiplode5(n)) {
+	// 	return "Fizz Buzz"
+	// }
+	if (fizzBuzz1.esFizz) {
 		return "Fizz";
 	}
-	if (esmultiplode5(n)) {
-		return "Buzz";
-	}
+	// if (esmultiplode5(n)) {
+	// 	return "Buzz";
+	// }
 
-	return n;
+	return fizzBuzz1.number;
 };
